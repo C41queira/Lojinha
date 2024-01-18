fbdada1a9d1729ff25acb79f63355b300bf89843
Caick <bertinviana@gmail.com>
2024-01-18 12:55:36 -0300

    Maven Project 2.0

---
+++ testecomerce/src/main/java/com/bigode/testecomerce/TestecomerceApplication.java
@@ -0,0 +1,13 @@
+package com.bigode.testecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestecomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestecomerceApplication.class, args);
+	}
+
+}
---
+++ testecomerce/src/test/java/com/bigode/testecomerce/TestecomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigode.testecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestecomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
