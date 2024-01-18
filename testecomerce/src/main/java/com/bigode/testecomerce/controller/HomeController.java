df8a2928ec38c7a3b1b5b85c93310ef204a7d86a
Caick <bertinviana@gmail.com>
2024-01-18 13:10:23 -0300

    Index html and home controller

---
+++ testecomerce/src/main/java/com/bigode/testecomerce/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.bigode.testecomerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView(); 
+		mv.setViewName("index");
+		
+		
+		
+		return mv; 
+	}
+}
